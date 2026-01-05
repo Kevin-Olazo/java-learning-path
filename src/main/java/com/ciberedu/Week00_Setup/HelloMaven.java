b297bb4e52c1df111b79c053b7f0c8f453a17350
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-05 00:21:14 -0500

    First commit, fixed workspace structure

---
+++ src/main/java/com/ciberedu/Week00_Setup/HelloMaven.java
@@ -0,0 +1,7 @@
+package com.ciberedu.Week00_Setup;
+
+public class HelloMaven {
+    public static void main(String[] args) {
+        System.out.println("Java Learning Path: Workspace Configured!");
+    }
+}
