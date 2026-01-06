f01d1502c7027afdb3a83bec4d35d617f20dfe0b
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-05 20:09:39 -0500

    completed Rectangle class, created ShoppingCart class

---
+++ src/main/java/com/ciberedu/Week01_Encapsulation/Day05_Practice/ShoppingCart.java
@@ -0,0 +1,29 @@
+package com.ciberedu.Week01_Encapsulation.Day05_Practice;
+
+public class ShoppingCart {
+    final private String customerId;
+    private double totalPrice;
+    private int itemCount;
+
+    public ShoppingCart(String customerId) {
+        this.customerId = customerId;
+        this.totalPrice = 0;
+        this.itemCount = 0;
+    }
+
+    void addItem() {
+
+    }
+
+    void removeItem() {
+
+    }
+
+    void clear() {
+
+    }
+
+
+
+
+}
