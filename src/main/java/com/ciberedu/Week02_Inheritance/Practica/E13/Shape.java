2096ec967f09b1f8dc44cdae1ad0a111bef2100d
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-07 16:20:22 -0500

    fix: Shape to abstract class

--- src/main/java/com/ciberedu/Week02_Inheritance/Practica/E13/Shape.java
+++ src/main/java/com/ciberedu/Week02_Inheritance/Practica/E13/Shape.java
@@ -1,13 +1,11 @@
 package com.ciberedu.Week02_Inheritance.Practica.E13;
 
-public class Shape {
+public abstract class Shape {
     private String color;
 
     public Shape(String color) {
         this.color = color;
     }
 
-    public double getArea(){
-        return 0.0;
-    }
+    public abstract double getArea();
 }
