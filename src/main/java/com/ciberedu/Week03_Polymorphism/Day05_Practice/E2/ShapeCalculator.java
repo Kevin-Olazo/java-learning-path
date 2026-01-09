ecc21a836e8225aa8f8fc6f8be18b901f2c90547
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-09 17:05:13 -0500

    feat: added ShapeCalculator class with methods, printShapeInfo and findLargestShape

--- src/main/java/com/ciberedu/Week03_Polymorphism/Day05_Practice/E2/ShapeCalculator.java
+++ src/main/java/com/ciberedu/Week03_Polymorphism/Day05_Practice/E2/ShapeCalculator.java
@@ -1,4 +1,36 @@
 package com.ciberedu.Week03_Polymorphism.Day05_Practice.E2;
 
 public class ShapeCalculator {
+
+    public void printShapeInfo(Shape[] shapes) {
+        if (shapes == null) {
+            throw new IllegalArgumentException("Shapes array cannot be null");
+        }
+
+        for (Shape sh : shapes) {
+            System.out.println(sh.calculateArea());
+        }
+    }
+
+    public Shape findLargestShape(Shape[] shapes) {
+        if (shapes == null) {
+            throw new IllegalArgumentException("Shapes array cannot be null");
+        }
+
+        Shape largestShape = shapes[0];
+        double largestArea = shapes[0].calculateArea();
+
+        for (int i = 1; i < shapes.length; i++) {
+            if (shapes[i] == null) {
+                break;
+            }
+
+            if (shapes[i].calculateArea() > largestArea) {
+                largestArea = shapes[i].calculateArea();
+                largestShape = shapes[i];
+            }
+        }
+
+        return largestShape;
+    }
 }
