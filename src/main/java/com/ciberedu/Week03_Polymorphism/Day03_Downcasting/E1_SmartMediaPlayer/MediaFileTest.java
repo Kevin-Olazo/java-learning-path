1742f0b849bd7e7513c2fc15857c20ddfbe43eeb
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-08 19:00:36 -0500

    Exercise, added analyzePlaylist method

--- src/main/java/com/ciberedu/Week03_Polymorphism/Day03_Downcasting/E1_SmartMediaPlayer/MediaFileTest.java
+++ src/main/java/com/ciberedu/Week03_Polymorphism/Day03_Downcasting/E1_SmartMediaPlayer/MediaFileTest.java
@@ -1,19 +1,46 @@
 package com.ciberedu.Week03_Polymorphism.Day03_Downcasting.E1_SmartMediaPlayer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MediaFileTest {
     public static void main(String[] args) {
 
-        MediaFile mf1 = new Video();
-        MediaFile mf2 = new Song();
+        MediaFile vf1 = new Video(false);
+        MediaFile vf2 = new Video(false);
+        MediaFile vf3 = new Video(true);
+        MediaFile vf4 = new Video(false);
+        MediaFile vf5 = new Video(true);
 
-        MediaFile[] mediaFiles = new MediaFile[10];
-        mediaFiles[0] = mf1;
-        mediaFiles[1] = mf2;
+        MediaFile sf1 = new Song(true);
+        MediaFile sf2 = new Song(true);
+        MediaFile sf3 = new Song(true);
+        MediaFile sf4 = new Song(false);
+        MediaFile sf5 = new Song(false);
+        MediaFile sf6 = new Song(true);
 
+        List<MediaFile> mediaFiles = new ArrayList<>();
 
+        mediaFiles.add(vf1);
+        mediaFiles.add(vf2);
+        mediaFiles.add(vf3);
+        mediaFiles.add(vf4);
+        mediaFiles.add(vf5);
+
+        mediaFiles.add(sf1);
+        mediaFiles.add(sf2);
+        mediaFiles.add(sf3);
+        mediaFiles.add(sf4);
+        mediaFiles.add(sf5);
+        mediaFiles.add(sf6);
+
+        MediaFileTest mft = new MediaFileTest();
+
+        mft.analyzePlaylist(mediaFiles);
     }
 
-    public  void previewMedia(MediaFile m){
+
+    public void previewMedia(MediaFile m) {
         if (m == null) {
             throw new IllegalArgumentException("File cannot be null");
         }
@@ -26,4 +53,24 @@ public class MediaFileTest {
             s.listen();
         }
     }
+
+    public void analyzePlaylist(List<MediaFile> playlist) {
+        int videos4k = 0;
+        int instrumentalSongs = 0;
+        for (MediaFile f : playlist) {
+            if (f instanceof Video v) {
+                if (v.is4K()) {
+                    videos4k++;
+                }
+            }
+            if (f instanceof Song s) {
+                if (s.isInstrumental()) {
+                    instrumentalSongs++;
+                }
+            }
+        }
+        System.out.println("4K Videos: " + videos4k + ", Instrumental: " + instrumentalSongs);
+    }
 }
+
+
--- src/main/java/com/ciberedu/Week03_Polymorphism/Day03_Downcasting/E1_SmartMediaPlayer/Video.java
+++ src/main/java/com/ciberedu/Week03_Polymorphism/Day03_Downcasting/E1_SmartMediaPlayer/Video.java
@@ -1,6 +1,15 @@
 package com.ciberedu.Week03_Polymorphism.Day03_Downcasting.E1_SmartMediaPlayer;
 
 public class Video extends MediaFile{
+    private boolean is4K;
+
+    public Video(boolean is4K) {
+        this.is4K = is4K;
+    }
+
+    public boolean is4K() {
+        return is4K;
+    }
 
     public void watch() {
         System.out.println("Watching video...");
