9d8442a6829c19d77d10de0be607afa9c19550eb
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-13 15:14:36 -0500

    feat: created MusicSubscription class

--- src/main/java/com/ciberedu/Week04_AbstractClasses/Day01_AbstractClasses/E2_StreamSubService/MusicSubscription.java
+++ src/main/java/com/ciberedu/Week04_AbstractClasses/Day01_AbstractClasses/E2_StreamSubService/MusicSubscription.java
@@ -1,4 +1,28 @@
 package com.ciberedu.Week04_AbstractClasses.Day01_AbstractClasses.E2_StreamSubService;
 
-public class MusicSubscription {
+public class MusicSubscription extends Subscription {
+    private boolean familyPlan;
+
+    public MusicSubscription(String subscriberName, double basePrice, String planType, boolean familyPlan) {
+        super(subscriberName, basePrice, planType);
+        this.familyPlan = familyPlan;
+    }
+
+    @Override
+    public double calculateMonthlyFee() {
+        if (familyPlan) {
+            return getBasePrice() + 10;
+        } else {
+            return getBasePrice();
+        }
+    }
+
+    @Override
+    public void accessContent(String title) {
+        System.out.println("Playing song: [title] (Family Shared: " + isFamilyPlan() +  ")" );
+    }
+
+    public boolean isFamilyPlan() {
+        return familyPlan;
+    }
 }
