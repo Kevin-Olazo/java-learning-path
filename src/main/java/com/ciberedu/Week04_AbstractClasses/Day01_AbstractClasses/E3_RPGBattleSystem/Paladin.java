78ea2983b8e7cbb71bb38e1f6a37e547e51a4710
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-13 15:38:28 -0500

    feat: Paladin class with specialAbility

---
+++ src/main/java/com/ciberedu/Week04_AbstractClasses/Day01_AbstractClasses/E3_RPGBattleSystem/Paladin.java
@@ -0,0 +1,32 @@
+package com.ciberedu.Week04_AbstractClasses.Day01_AbstractClasses.E3_RPGBattleSystem;
+
+public class Paladin extends GameCharacter{
+    private int faithPoints;
+
+    public Paladin(String name, int health, int faithPoints) {
+        super(name, health);
+        this.faithPoints = faithPoints;
+    }
+
+    @Override
+    public void attack(GameCharacter opponent) {
+        if (opponent.getHealth() > 0) {
+            opponent.takeDamage(10);
+            faithPoints += 5;
+        } else {
+            System.out.println("Opponent has already been defeated");
+        }
+
+    }
+
+    @Override
+    public void specialAbility() {
+        if (faithPoints >= 20) {
+            setHealth(getHealth() + 15);
+            System.out.println("Healed 15HP!");
+        } else {
+            System.out.println("Not enough faith!");
+        }
+
+    }
+}
