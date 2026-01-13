1451655792195ae82a55313ece4754f63d5d125c
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-13 16:33:38 -0500

    Day01E4 - Secure Banking System

---
+++ src/main/java/com/ciberedu/Week04_AbstractClasses/Day01_AbstractClasses/E4_SecureBankingSystem/BankAccount.java
@@ -0,0 +1,36 @@
+package com.ciberedu.Week04_AbstractClasses.Day01_AbstractClasses.E4_SecureBankingSystem;
+
+public abstract class BankAccount {
+    private String accountNumber;
+    private double balance;
+    private String holderName;
+
+    public BankAccount(String accountNumber, double balance, String holderName) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.holderName = holderName;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Enter a valid amount to deposit");
+        }
+
+        this.balance += amount;
+        System.out.println("Deposited: $" + amount);
+
+    }
+
+    public void transfer(BankAccount target, double amount) {
+        if (withdraw(amount)) {
+            target.deposit(amount);
+        } else {
+            System.out.println("Transfer failed");
+        }
+    }
+
+    public abstract boolean withdraw(double amount);
+
+    public abstract void applyEndOfMonth();
+
+}
