539ce590fbd712b8a53356217f8f76684d7f115a
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-13 21:35:40 -0500

    feat: ExcelReport, HTMLReport classes

--- src/main/java/com/ciberedu/Week04_AbstractClasses/Day02_TemplatePattern/ExcelReport.java
+++ src/main/java/com/ciberedu/Week04_AbstractClasses/Day02_TemplatePattern/ExcelReport.java
@@ -1,19 +1,19 @@
 package com.ciberedu.Week04_AbstractClasses.Day02_TemplatePattern;
 
-public class PDFReport extends Report {
+public class ExcelReport extends Report{
 
     @Override
     public void formatHeader() {
-        System.out.println("------- PDF HEADER -------");
+
     }
 
     @Override
     public void formatBody() {
-        System.out.println("      [ PDF BODY ]     ");
+
     }
 
     @Override
     public void formatFooter() {
-        System.out.println("======= PDF FOOTER =======");
+
     }
 }
--- src/main/java/com/ciberedu/Week04_AbstractClasses/Day02_TemplatePattern/Report.java
+++ src/main/java/com/ciberedu/Week04_AbstractClasses/Day02_TemplatePattern/Report.java
@@ -2,7 +2,9 @@ package com.ciberedu.Week04_AbstractClasses.Day02_TemplatePattern;
 
 public abstract class Report {
     public final void generateReport() {
-
+        formatHeader();
+        formatBody();
+        formatFooter();
     }
 
     public abstract void formatHeader();
