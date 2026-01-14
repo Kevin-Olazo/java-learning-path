f15c76aefc5f0997643929f22364555b5ea1f5e6
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-13 21:05:42 -0500

    fix: moved PDFReport content to Report abstract class

--- src/main/java/com/ciberedu/Week04_AbstractClasses/Day02_TemplatePattern/PDFReport.java
+++ src/main/java/com/ciberedu/Week04_AbstractClasses/Day02_TemplatePattern/PDFReport.java
@@ -1,12 +1,6 @@
 package com.ciberedu.Week04_AbstractClasses.Day02_TemplatePattern;
 
-public abstract class PDFReport {
+public class PDFReport {
 
-    public final void generateReport() {
 
-    }
-
-    public abstract void formatHeader();
-    public abstract void formatBody();
-    public abstract void formatFooter();
 }
--- src/main/java/com/ciberedu/Week04_AbstractClasses/Day02_TemplatePattern/Report.java
+++ src/main/java/com/ciberedu/Week04_AbstractClasses/Day02_TemplatePattern/Report.java
@@ -1,4 +1,11 @@
 package com.ciberedu.Week04_AbstractClasses.Day02_TemplatePattern;
 
 public abstract class Report {
+    public final void generateReport() {
+
+    }
+
+    public abstract void formatHeader();
+    public abstract void formatBody();
+    public abstract void formatFooter();
 }
