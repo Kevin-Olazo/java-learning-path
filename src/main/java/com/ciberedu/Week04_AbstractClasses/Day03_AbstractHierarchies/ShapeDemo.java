cdecf37023c20bc986d651eafd66fee7593f9266
Kevin-Olazo <kevind.olazo@gmail.com>
2026-01-16 15:54:55 -0500

    feat: ShapeDemo class to test

--- src/main/java/com/ciberedu/Week04_AbstractClasses/Day03_AbstractHierarchies/ShapeDemo.java
+++ src/main/java/com/ciberedu/Week04_AbstractClasses/Day03_AbstractHierarchies/ShapeDemo.java
@@ -1,4 +1,20 @@
 package com.ciberedu.Week04_AbstractClasses.Day03_AbstractHierarchies;
 
+import java.util.ArrayList;
+
 public class ShapeDemo {
+    public static void main(String[] args) {
+        ArrayList<Shape> shapes = new ArrayList<>();
+
+        shapes.add(new Triangle("blue", 70, 100));
+
+        shapes.add(new Circle("red",50));
+
+        for (Shape shape : shapes) {
+            shape.displayInfo();
+            System.out.println(shape.calculateArea());
+        }
+
+    }
+
 }
