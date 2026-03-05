f28f95a6ce6ec24559b6457914056c34b20424e8
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-04 22:30:13 -0500

    feat: create generic classes Type and Pair

---
+++ src/main/java/com/ciberedu/week09_GenericsOptionals/day01_generic_classes/Vehicle.java
@@ -0,0 +1,17 @@
+package com.ciberedu.week09_GenericsOptionals.day01_generic_classes;
+
+public class Vehicle {
+    public String model;
+
+    public Vehicle(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
