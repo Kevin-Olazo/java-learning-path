83ed08797e883e3ffa68a193e7d831dc1b521bad
Kevin-Olazo <kdaniel.olazo@gmail.com>
2026-03-09 15:09:20 -0500

    feat: added logic to optional methods

--- src/main/java/com/ciberedu/MainTest.java
+++ src/main/java/com/ciberedu/MainTest.java
@@ -11,40 +11,21 @@ import static java.util.stream.Collectors.toList;
 class MainTest {
 
     public static void main(String[] args) throws IOException {
-        // 1. Crea scanner
-        Scanner in = new Scanner(System.in);
-        // 2. Obtenemos el numero de contactos (n)
-        int n = in.nextInt();
-        in.nextLine();
-        // 3. Creo el mapa para agregar los contactos
-        Map<String, Integer> phoneBook = new HashMap<>();
+        System.out.println("Factorial de 0: " + factorial(0));
+        System.out.println("Factorial de 1: " + factorial(1));
+        System.out.println("Factorial de 2: " + factorial(2));
+        System.out.println("Factorial de 3: " + factorial(3));
+        System.out.println("Factorial de 5: " + factorial(5));
 
-        // 4. Itero la cantidad de veces (n) para agregar contactos
-        for(int i = 0; i < n; i++){
-            // 4.1. Obtengo el nombre
-            String name = in.next();
-            // 4.2. Obtengo el telefono
-            int phone = in.nextInt();
-            // Write code here
-            in.nextLine();
-            phoneBook.put(name, phone);
-        }
 
-        // 5. While loop para ingresar busqueda de nombre para obtener contacto
-        while(in.hasNext()){
-            // 5.1. Obtengo el nombre de contacto
-            String s = in.next();
-            // Write code here
-            if(phoneBook.containsKey(s)){
-                System.out.println(s + "=" + phoneBook.get(s));
-            } else {
-                System.out.println("Not found!");
-            }
-
-        }
-
-        in.close();
     }
+
+    //Recursive
+    public static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
+
 
 
 }
--- src/main/java/com/ciberedu/week09_GenericsOptionals/day04_optional_basics/CartManager.java
+++ src/main/java/com/ciberedu/week09_GenericsOptionals/day04_optional_basics/CartManager.java
@@ -13,30 +13,64 @@ public class CartManager {
     // Si lo encuentras, retorna Optional.of(). Si no, retorna Optional.empty().
     public Optional<Product> findProductById(String id) {
         // Tu código aquí
-        return null; // Cambia esto
+        for(Product p : cart.getProducts()) {
+            if(p.getId().equals(id)) {
+                return Optional.of(p);
+            }
+        }
+        return Optional.empty();
     }
 
     // TODO 2: Encuentra el producto más barato.
     // Retorna un Optional.
     public Optional<Product> findCheapest() {
         // Tu código aquí
-        return null; // Cambia esto
+        if (cart.getProducts().isEmpty()){
+            return Optional.empty();
+        }
+
+        Product cheapest = cart.getProducts().get(0);
+        for(Product p : cart.getProducts()){
+            if (p.getPrice() < cheapest.getPrice()){
+                cheapest = p;
+            }
+        }
+
+        return Optional.of(cheapest);
+
     }
 
     // TODO 3: Usando tu método findProductById, demuestra las 4 formas de extraer/verificar.
     public void demonstrateOptional() {
         // 1. Usa isPresent() o isEmpty() con un IF tradicional para imprimir si existe el ID "A1"
-
+        if (findProductById("A1").isPresent()){
+            System.out.println("El ID A1 existe");
+        }
         // 2. Intenta buscar el ID "INVENTADO". Usa orElse() para guardar un producto por defecto ("Generico", precio 0) en una variable.
+        Product generico = new Product("B001", "Generico", 10.00, "INVENTADO");
+        Product productoSeguro = findProductById("INVENTADO").orElse(generico);
+
 
         // 3. Intenta buscar el ID "ERROR". Usa orElseThrow() para lanzar IllegalArgumentException si no existe.
+        findProductById("ERROR").orElseThrow(() -> new IllegalArgumentException("No existe"));
 
         // 4. Busca el ID "A1" y usa ifPresent() con una Lambda para imprimir su nombre en una sola línea.
+        findProductById("A1").ifPresent( (p) -> System.out.println(p.getName()) );
     }
 
     // TODO 4: Compara. Escribe un método rápido findCheapestOldSchool() que retorne Product (o null) sin usar Optional, para contrastar.
     public Product findCheapestOldSchool() {
         // Tu código aquí
-        return null;
+        if (cart.getProducts().isEmpty()){
+            return null;
+        }
+        Product cheapest = cart.getProducts().get(0);
+        for(Product p : cart.getProducts()){
+            if (p.getPrice() < cheapest.getPrice()){
+                cheapest = p;
+            }
+        }
+
+        return cheapest;
     }
 }
